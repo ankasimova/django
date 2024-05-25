a4ca66de7220613a0626f3eb5e05b5ca18b19ff1
ankasimova <ak1722@yandex.ru>
2024-05-25 20:56:57 +0500

    http response

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotFound
 
 def index(request):
     host = request.META["HTTP_HOST"]
@@ -12,9 +12,9 @@ def index(request):
     """)
 
 def main(request):
-    return HttpResponse("Главная")
+    return HttpResponse("Главная", status=400, reason="Incorrect Request")
 
-def about(request, name, age):
+def user(request, name='Не введено', age=0):
 
     return HttpResponse(f"""
             <h2>О пользователе</h2>
@@ -22,5 +22,44 @@ def about(request, name, age):
             <p>Возраст: {age}</p>
     """)
 
+#передача через query params в формате http://127.0.0.1:8000/aboutuser/?name=Lika&age=27
+def aboutuser(request):
+    name = request.GET.get('name', 'Не передано')
+    age = request.GET.get('age', 0)
+    return HttpResponse(f"<h2>Имя: {name} Возраст: {age}</h2>")
+
+
 def contact(request):
     return HttpResponse("Контакты")
+
+def products(request, id):
+    return HttpResponse(f"Продукт {id}")
+
+def top(request):
+    return HttpResponse("Топ товаров")
+
+def new(request):
+    return HttpResponse("Новые товары")
+
+def comments(request, id):
+    return HttpResponse(f"Комментарии о товаре {id}")
+
+def questions(request, id):
+    return HttpResponse(f'Вопросы о товаре {id}')
+
+
+#Обработка кодов ошибок
+def index(request, id):
+    people = ["Данияр", "Диас", "Даулет"]
+    if id in range(0, len(people)):
+        return HttpResponse(people[id])
+    else:
+        return HttpResponseNotFound("Пользователь не найден")
+
+def age(request, age):
+    if age not in range(1, 111):
+        return HttpResponseBadRequest("Возраст указан некорректно")
+    elif age < 18:
+        return HttpResponseForbidden("Лицам до 18 лет доступ запрещен")
+    else:
+        return HttpResponse(f"Доступ разрешен. Возраст: {age}")
--- metanit/metanit/urls.py
+++ metanit/metanit/urls.py
@@ -14,12 +14,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, re_path
+
+from django.urls import path, re_path, include
 from hello import views
 
+product_patterns = [
+    path('', views.products),
+    path('new', views.new),
+    path('top', views.top),
+    path('comments', views.comments),
+    path('questions', views.questions)
+]
+
 urlpatterns = [
-    re_path('^about', views.about, kwargs={"name": "Lika", "age": "27"}),
+    path('user/<str:name>', views.user),
+    path('user/<str:name>/<int:age>', views.user),
     re_path('^contact', views.contact),
     re_path('^main', views.main),
     path('', views.index, name='home'),
+    path('products/', include(product_patterns)),
+    path('products/<int:id>/', include(product_patterns)),
+    path('aboutuser/', views.aboutuser),
+    path("index/<int:id>", views.index),
+    path("age/<int:age>", views.age)
 ]
